35f205bbe2ae08c6422a337f61b905ed3fe3fee9
LeoMurjinschi <leomurjinschi@gmail.com>
2024-09-29 12:20:52 +0200

    made some visual changes to the register page

--- react-app/src/Pages/Register.jsx
+++ react-app/src/Pages/Register.jsx
@@ -2,18 +2,30 @@ import React from 'react'
 import Logo from '../assets/logo.svg'
 
 export const Register = () => {
-  return (
-    <>
-        <div className='container mx-auto'>
-            <div>
-                <img src="logo.svg" alt="" />
-                <span>Skin Firts</span>
-                <p>Dermatology Center</p>
+    return (
+        <>
+            <div className='container mx-auto'>
+                <div className='flex flex-col justify-center items-center h-screen gap-[20px]'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <img className='' src={Logo} alt="" />
+                        <span className=''>Skin Firts</span>
+                        <p>Dermatology Center</p>
+                    </div>
+
+                    <div className='flex flex-col justify-center items-center max-w-[500px]'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates est ipsa quasi quidem magnam animi,
+                            totam et iste aliquid a tenetur, illum necessitatibus aspernatur, perspiciatis perferendis obcaecati
+                            repellendus nesciunt quis!</p>
+                        <div className='flex flex-col gap-[14px] mt-12'>
+                            <button className='px-[40px] py-[23px] text-white bg-[#2260FF] rounded-full'>Log In</button>
+                            <button className='px-[40px] py-[23px] text-[#2260FF] bg-[#CAD6FF] rounded-full'>Sign Up</button>
+                        </div>
+                    </div>
+
+
+                </div>
+
             </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates est ipsa quasi quidem magnam animi, totam et iste aliquid a tenetur, illum necessitatibus aspernatur, perspiciatis perferendis obcaecati repellendus nesciunt quis!</p>
-            <button></button>
-            <button></button>
-        </div>
-    </>
-  )
+        </>
+    )
 }
