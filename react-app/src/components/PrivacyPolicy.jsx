83c26507de992565b88f0bb7953db2d45b1a499b
BEN1ZK <134963857+BEN1ZK@users.noreply.github.com>
2024-10-04 22:28:19 +0300

    Privacy Policy Component

---
+++ react-app/src/components/PrivacyPolicy.jsx
@@ -0,0 +1,19 @@
+const PrivacyPolicy = () => {
+    return (
+      <div className="p-4">
+        <h1 className="text-xl font-bold">Privacy Policy</h1>
+        <p>Last Update: 14/08/2024</p>
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin at diam quam...
+        </p>
+        <h2 className="mt-8 font-bold">Terms & Conditions</h2>
+        <ol className="mt-2">
+          <li>Lorem ipsum dolor sit amet, consectetur...</li>
+          <li>Phasellus nec sem ut lectus faucibus...</li>
+        </ol>
+      </div>
+    );
+  };
+  
+  export default PrivacyPolicy;
+  
